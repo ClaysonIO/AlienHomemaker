a45c214c87abf5f0c18a8ea5a705815af816d868
Tao Liu <tao@liftoff.io>
2019-01-26 13:10:35 -0800

    Add placeholder interaction for EatScene

--- src/EatScene.js
+++ src/EatScene.js
@@ -17,10 +17,17 @@ export const EatScene = new Phaser.Class({
 
     create: function ()
     {
+        const farmScene = this.scene.get("farmScene");
+        const person1 = this.add.sprite(200, 300, 'p1')
+            .setInteractive()
+            .setDataEnabled()
+            .on("pointerdown", () => {
+                farmScene.removePerson(this.data.get("name"));
+                this.scene.switch("farmScene");
+            });
     },
 
     update: function (time, delta)
     {
-    }
-
+    },
 });
