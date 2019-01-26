d1684f44ff19bf794b2a4f5c052412963cc07561
Tao Liu <tao@liftoff.io>
2019-01-26 11:24:31 -0800

    Placeholder end screen + timeout to start screen

--- src/EndScene.js
+++ src/EndScene.js
@@ -13,14 +13,17 @@ export const EndScene = new Phaser.Class({
 
     preload: function ()
     {
+        this.load.image("shooter", "assets/shooter.png");
+        this.load.image("dead", "assets/dead.png");
     },
 
     create: function ()
     {
+        this.add.sprite(300,400,"shooter");
+        this.add.sprite(700,500,"dead");
+        const text = this.add.text(250, 150, 'You have failed us!', { font: '50px Arial', fill: '#00ff00' });
+        setTimeout(() => {
+            this.scene.start("startScene");
+        }, 3000);
     },
-
-    update: function (time, delta)
-    {
-    }
-
 });
