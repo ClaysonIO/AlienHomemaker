264ccd7a7da002f185cf0bad4a94493362f8f9e2
Daniel Clayson <danclayson@gmail.com>
2019-01-26 10:19:47 -0800

    Basic Person

--- src/person.js
+++ src/person.js
@@ -1,15 +1,33 @@
+import faker from 'faker'
+
 export class Person {
   constructor(){
-    this.name = 'Person Name';
-    this.attributes = [];
-    this.happiness = .5;
-    this.age = Math.random() * 80;
-    this.gender = 0;
+    this.gender = faker.random.number(1); // 0 is male, 1 is female
+    this.name = faker.name.findName(null, null, this.gender);
+    this.r = Math.floor(Math.random() * 256);
+    this.g = Math.floor(Math.random() * 256);
+    this.b = Math.floor(Math.random() * 256);
+    this.a = .5;
 
-    for(let i = 0; i < 5; i++){
-      this.attributes.push(Math.random() * 100);
-    }
+    this.priority = Math.random.arrayElements(['r', 'g', 'b']);
+
+    this.x = null; //Current position in the farm
+    this.y = null;
+
+    this.velocity = null; //The current movement velocity of this individual
   }
 
+  updateHappiness(){
 
+  }
+
+  calculateHappiness(otherPerson){
+
+  }
+
+  calculateVelocity(allOtherPeople){
+
+  }
+
+  movePerson
 }
