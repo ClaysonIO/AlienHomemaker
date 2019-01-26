193609d3e335f8e397b0f5b1b603e369ef3d6538
Tao Liu <tao@liftoff.io>
2019-01-26 10:03:50 -0800

    Initialize scene files for game

---
+++ src/SelectionScene.js
@@ -0,0 +1,26 @@
+export const SelectionScene = new Phaser.Class({
+
+    Extends: Phaser.Scene,
+
+    initialize:
+
+        function SelectionScene ()
+        {
+            Phaser.Scene.call(this, {
+                key: 'selectionScene',
+            });
+        },
+
+    preload: function ()
+    {
+    },
+
+    create: function ()
+    {
+    },
+
+    update: function (time, delta)
+    {
+    }
+
+});
---
+++ src/StartScene.js
@@ -0,0 +1,26 @@
+export const StartScene = new Phaser.Class({
+
+    Extends: Phaser.Scene,
+
+    initialize:
+
+        function StartScene ()
+        {
+            Phaser.Scene.call(this, {
+                key: 'startScene',
+            });
+        },
+
+    preload: function ()
+    {
+    },
+
+    create: function ()
+    {
+    },
+
+    update: function (time, delta)
+    {
+    }
+
+});
