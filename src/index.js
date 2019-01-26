1915bf512f2514c25d9b3c403e639f3648360066
Daniel Clayson <danclayson@gmail.com>
2019-01-26 15:02:01 -0800

    Add Physics, Add experimental scene

--- src/index.js
+++ src/index.js
@@ -4,17 +4,25 @@ import {StartScene} from "./StartScene";
 import {FarmScene} from "./FarmScene";
 import {EatScene} from "./EatScene";
 import {EndScene} from "./EndScene";
+import {ExperimentalScene} from "./ExperimentalScene_DC";
 
 export const width = 1024;
 export const height = 768;
 
 const config = {
-    type: Phaser.AUTO,
-    width,
-    height,
-    backgroundColor: '#182d3b',
-    parent: 'phaser-example',
-    scene: [ StartScene, SelectionScene, FarmScene, EatScene, EndScene ]
+  type: Phaser.AUTO,
+  width,
+  height,
+  backgroundColor: '#182d3b',
+  parent: 'phaser-example',
+  physics: {
+    default: 'impact',
+    impact: {
+      debug: true,
+    }
+  },
+  scene: [ StartScene, SelectionScene, FarmScene, EatScene, EndScene ]
+  // scene: [ ExperimentalScene ]
 };
 
 const game = new Phaser.Game(config);
