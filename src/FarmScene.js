c917f88ac12ac6938019095654a9dfd4e8998cce
Tao Liu <tao@liftoff.io>
2019-01-27 13:04:34 -0800

    Remove EndScene delay + hide debug

--- src/FarmScene.js
+++ src/FarmScene.js
@@ -120,7 +120,7 @@ export const FarmScene = new Phaser.Class({
           .setFill(this.isMealTime ? dangerColor : textColor);
       this.populationText.setText(`Population: ${everybody.length}`);
       if (this.happiness < 0.15) {
-        this.time.delayedCall(1000, () => this.scene.start("EndScene"), [], this)
+        this.scene.start("EndScene");
       }
     }
   },
